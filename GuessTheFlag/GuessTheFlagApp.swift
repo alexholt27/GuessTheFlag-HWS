68c72852f572a6ee779c5fe575e896b4f0477cdc
Alex Holt <alex.holttt@gmail.com>
2025-01-09 12:40:33 -0600

    initial commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Alex Holt on 1/8/25.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
